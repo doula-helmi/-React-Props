717fbe114284e65ef99256d080a6f47a4241cf45
doula-helmi <doula.helmi@gmail.com>
2022-07-18 15:00:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from "./Profile/Profile";
+import image from "./helmi.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Profile
+      fullName="Doula Helmi"
+      Bio="La vie ne se mesure pas par le nombre de respirations, mais par les moments qui nous coupent le souffle."
+      Profession="Fullstack JS"
+    >
+      {image}
+    </Profile>
   );
 }
 
---
+++ src/Profile/Profile.js
@@ -0,0 +1,68 @@
+import propTypes from 'prop-types';
+
+
+const containerStyle ={
+  backgroundColor: '#231E39',
+	borderRdius: '5px',
+	boxShadow: '0px 10px 20px -10px rgba(0,0,0,0.75)',
+	color: '#B3B8CD',
+	paddingTop: '30px',
+  paddingBottom: '30px',
+	position: 'relative',
+	width: '350px',
+	maxWidth: '100%',
+	textAlign: 'center',
+  borderRadius:'1rem'
+}
+const ButtonStyle = {
+  cursor: 'pointer',
+	backgroundColor: '#03BFCB',
+	border: '1px solid #03BFCB',
+	borderRadius: '3px',
+	color: '#231E39',
+	fontFamily: 'Montserrat, sans-serif',
+	fontWeight: '500',
+	padding: '10px 25px'
+}
+
+const displayname =(name) => {
+  alert(`My name is ${name}`);
+};
+
+const Profile = (props) => {
+  return (
+    <>
+      <div style={containerStyle}>
+        <img style={{width:'11rem',height:'11rem'}}
+          src={props.children}
+          alt="User logo"
+        />
+        <h1 style={{margin:'10px 0'}}>{props.FullName}</h1>
+        <h4>{props.Profession}</h4>
+        <p style={{fontSize:'14px',lineHeight:'21px'}}>
+        {props.Bio}
+        </p>
+        <div className="buttons">
+          <button style={ButtonStyle} onClick={()=>displayname(props.fullName)}>Click Here</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+Profile.propTypes = {
+  FullName: propTypes.string,
+  children: propTypes.string,
+  Profession: propTypes.string,
+  Bio: propTypes.string,
+  displayname: propTypes.func
+}
+
+Profile.defaultProps = {
+  FullName:"Doula Helmi",
+  Bio:"La vie ne se mesure pas par le nombre de respirations, mais par les moments qui nous coupent le souffle",
+  Profession:"Fullstack JS"
+}
+
+
+export default Profile;
